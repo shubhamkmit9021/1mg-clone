e33e151a12c1f7c6f8752d34cbb451fb3e79f582
shubhamkmit9021 <shubhamkmit9021@gmail.com>
2022-10-12 23:12:13 +0530

    db.json folder updated

--- 1mg-clone-app/src/App.jsx
+++ 1mg-clone-app/src/App.jsx
@@ -1,9 +1,15 @@
 
 import { Box } from '@chakra-ui/react';
 import './App.css';
+import Footer2 from './Components/Footer/Footer2';
+import Footer1 from './Components/Footer/Footer1';
+import { UpperHeading } from './Components/Middle/UpperHeading';
+import { UpperMiddle } from './Components/Middle/UpperMiddle';
+import UpperPoster from './Components/Middle/UpperPoster';
 import LowerNav from './Components/Navbar/LowerNav';
 import MiddleNav from './Components/Navbar/MiddleNav';
 import UpperNav from './Components/Navbar/UpperNav';
+import Footer3 from './Components/Footer/Footer3';
 
 
 function App() {
@@ -15,6 +21,14 @@ function App() {
       <hr />
       <LowerNav />
       <hr />
+      <UpperMiddle/>
+      <UpperHeading/>
+      <hr />
+      <UpperPoster/>
+      <hr />
+      <Footer1/>
+      <Footer2/>
+      <Footer3/>
     </Box>
   );
 }
