65ed02082744119cf849ed772f971c44c4bfad05
shubhamkmit9021 <shubhamkmit9021@gmail.com>
2022-10-13 19:45:10 +0530

    some features updated

--- 1mg-clone-app/src/App.jsx
+++ 1mg-clone-app/src/App.jsx
@@ -10,6 +10,8 @@ import LowerNav from './Components/Navbar/LowerNav';
 import MiddleNav from './Components/Navbar/MiddleNav';
 import UpperNav from './Components/Navbar/UpperNav';
 import Footer3 from './Components/Footer/Footer3';
+import Carousel  from "./Components/Carousel/Carousel.jsx"
+import { Sec1Img } from './Components/GetData/Sec1Img';
 
 
 function App() {
@@ -29,6 +31,9 @@ function App() {
       <Footer1/>
       <Footer2/>
       <Footer3/>
+      <hr />
+      <Carousel />
+      {/* <Sec1Img/> */}
     </Box>
   );
 }
