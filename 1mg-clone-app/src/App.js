67cb3a23cbe832ffecc069685c91f6eaba37577e
AMahakud07 <abhishekmahakud007@gmail.com>
2022-10-13 19:58:52 +0530

    carousel

--- 1mg-clone-app/src/App.js
+++ 1mg-clone-app/src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Login from "./Components Abhishek/login"
+// import ReactDOM from "react-dom";
+// import MyOwlCarousel from './Components Abhishek/owl';
 
 function App() {
   return (
     <div className="App">
       <Login/>
+      {/* <MyOwlCarousel/> */}
     </div>
   );
 }
