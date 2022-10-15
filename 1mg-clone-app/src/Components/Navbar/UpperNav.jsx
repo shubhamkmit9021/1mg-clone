0c257a9eee3dbf855c4f886d3c532729a5ced6c9
shubhamkmit9021 <shubhamkmit9021@gmail.com>
2022-10-15 22:51:11 +0530

    Navbar done with responsive

--- 1mg-clone-app/src/Components/Navbar/UpperNav.jsx
+++ 1mg-clone-app/src/Components/Navbar/UpperNav.jsx
@@ -1,28 +1,53 @@
-import { Box, Flex, HStack, Image, Text, Spacer } from "@chakra-ui/react";
+import { Box, Flex, HStack, Image, Text, Spacer, Show } from "@chakra-ui/react";
 import React from "react";
 import { GrCart } from "react-icons/gr";
+import LowerNav from "./LowerNav";
+import MiddleNav from "./MiddleNav";
+
 const UpperNav = () => {
   return (
-    <HStack p={1}>
-      <Flex flexWrap='wrap' w="72%"  alignItems='center'>
-        <Box px={3} ><Image src="https://www.1mg.com/images/tata_1mg_logo.svg" alt="logo" _hover={{ cursor:'pointer' }} /></Box>
-        <Box px={3} > <Text color='tomato' as='b' _hover={{ cursor:'pointer' }} > MEDICINES </Text> </Box>
-        <Box px={3} > <Text fontSize='16px' as='b'  _hover={{ color:'tomato', cursor:'pointer' }} > LAB TESTS </Text> </Box>
-        <Box px={3} > <Text fontSize='16px' as='b'  _hover={{ color:'tomato', cursor:'pointer' }} > CONSULT DOCTORS </Text> </Box>
-        <Box px={3} > <Text fontSize='16px' as='b'  _hover={{ color:'tomato', cursor:'pointer' }} > COVID-19 </Text> </Box>
-        <Box px={3} > <Text fontSize='16px' as='b'  _hover={{ color:'tomato', cursor:'pointer' }} > AYURVEDA </Text> </Box>
-        <Box px={3} > <Text fontSize='16px' as='b'  _hover={{ color:'tomato', cursor:'pointer' }} > CARE PLAN </Text> </Box> 
+    <>
+      <HStack py='1' px='1' mx='1' >
+      <Flex flexWrap='wrap'  w={{ base: '45%', sm: '45%', md: '30%', lg: '70%', xl: '72%'}} mr='2'  alignItems='center' justifyContent='space-evenly'>
+        <Box><Image src="https://www.1mg.com/images/tata_1mg_logo.svg" alt="logo" _hover={{ cursor:'pointer' }} w='100px' /></Box>
+        <Show above='lg'>
+        <Box> <Text as='b' _hover={{ cursor:'pointer' }}  color='tomato' > MEDICINES </Text> </Box>
+        <Box> <Text as='b' _hover={{ color:'tomato', cursor:'pointer' }} > LAB TESTS </Text> </Box>
+        <Box> <Text as='b' _hover={{ color:'tomato', cursor:'pointer' }} > CONSULT DOCTORS </Text> </Box>
+        <Box> <Text as='b' _hover={{ color:'tomato', cursor:'pointer' }} > COVID-19 </Text> </Box>
+        <Box> <Text as='b' _hover={{ color:'tomato', cursor:'pointer' }} > AYURVEDA </Text> </Box>
+        <Show above='xl'>
+        <Box> <Text as='b' _hover={{ color:'tomato', cursor:'pointer' }} > CARE PLAN </Text> </Box> 
+        </Show>
+        </Show>
       </Flex>
       <Spacer />
-      <Flex flexWrap='wrap' w="25%"  alignItems='center'>
-      {/* <Spacer /> */}
-        <Box> <Text fontSize='14px' _hover={{ cursor:'pointer' }}  > Login | Sign Up </Text> </Box>
-        <Box px={3} ><Text fontSize='14px' _hover={{ cursor:'pointer' }}  > Offers </Text> </Box>
-        <Box px={3} fontSize='24px' _hover={{ cursor:'pointer' }} ><GrCart /></Box>
-        <Box px={3} ><Text fontSize='14px' _hover={{ cursor:'pointer' }} > Need Help? </Text> </Box>
+      <Flex flexWrap='wrap'  w={{ base: '55%', sm: '50%', md: '30%', lg: '25%', xl: '26%'}} alignItems='center' justifyContent='space-evenly'>
+        <Box><Text fontSize={{ base: '16px', sm: '16px', md: '18px', lg: '16px', xl: "14px" }} _hover={{ cursor:'pointer' }} > Login | Sign Up </Text> </Box>
+        <Show above='lg'>
+        <Box><Text _hover={{ cursor:'pointer' }} > Offers </Text> </Box>
+        </Show>
+        <Box fontSize={{ base: '24px', sm: '24px', md: '24px', lg: '24px', xl: "24px" }} _hover={{ cursor:'pointer' }} ><GrCart /></Box>
+        <Show above='xl'>
+        <Box><Text _hover={{ cursor:'pointer' }} > Need Help? </Text> </Box>
+        </Show>
       </Flex>
      
     </HStack>
+
+    <Box mt='1' mb='0.5'>
+      <hr />
+    </Box>
+
+    <MiddleNav/>
+
+    <Box mt='1' mb='0.5'>
+      <hr />
+    </Box>
+    
+    <LowerNav/>
+    
+    </>
   );
 };
 
